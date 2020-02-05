7c1b139c8a2159a0537ae19d036f4a2e01f04fa8
tqdat99 <tqdat99@gmail.com>
2020-02-05 20:29:32 +0700

    Single Page

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//     res.render('home');
+// });
+
+// router.get('/projects', function(req, res, next) {
+//     res.render('projects');
+// });
+
+// router.get('/about', function(req, res, next) {
+//     res.render('about');
+// });
+
+// router.get('/contact', function(req, res, next) {
+//     res.render('contact');
+// });
+
+// router.get('/projects/my-very-first-step-from-motion-design-to-ui-design', function(req, res, next) {
+//     res.render('New look new story xotours vn');
+// });
+
 router.get('/', function(req, res, next) {
-    res.render('home');
-});
-
-router.get('/projects', function(req, res, next) {
-    res.render('projects');
-});
-
-router.get('/about', function(req, res, next) {
-    res.render('about');
-});
-
-router.get('/contact', function(req, res, next) {
-    res.render('contact');
-});
-
-router.get('/projects/my-very-first-step-from-motion-design-to-ui-design', function(req, res, next) {
-    res.render('New look new story xotours vn');
-});
-
-router.get('/test', function(req, res, next) {
-    res.render('test');
+    res.render('tqdat');
 })
 
 module.exports = router;
