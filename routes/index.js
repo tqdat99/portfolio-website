f6d046867d32d77788c3dc61cfa09c8c65d56cfd
unknown <tqdat99@gmail.com>
2021-10-05 19:44:04 +0700

    Update 5, Ocr

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('home');
 });
 
-router.get('/works', function(req, res, next) {
+router.get('/works', function (req, res, next) {
     res.render('works');
 });
 
-router.get('/about', function(req, res, next) {
-    res.render('about');
-});
-
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
     res.render('contact');
 });
-
-// router.get('/test', function(req, res, next) {
-//     res.render('tqdat');
-// })
-
-// router.get('/test2', function(req, res, next) {
-//     res.render('test');
-// })
 
 module.exports = router;
